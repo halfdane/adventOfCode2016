b157fc841749183db76f95939121cf0eccfeaa68
halfdane <halfdane@gmx.net>
2016-12-19 10:26:43 +0100

    day06p2

---
+++ lib/day06/p2.ex
@@ -0,0 +1,58 @@
+ExUnit.start
+
+defmodule Day06_2 do
+    use ExUnit.Case, async: true
+
+    test "input1" do
+      input = read_file("input_01.txt")
+      result = solve(input)
+      IO.puts "Result is #{result|>inspect}"
+      assert result == "advent"
+    end
+
+    test "result" do
+      input = read_file("input.txt")
+            result = solve(input)
+            IO.puts "Result is #{result}"
+            assert result == "aovueakv"
+    end
+
+    def solve(input) do
+      input
+        |> Enum.map(&String.codepoints/1)
+        |> Enum.reduce(%{}, &collate/2)
+        |> Enum.sort(&(elem(&1,0)<elem(&2,0)))
+        |> Enum.map(&(count(elem(&1, 1))))
+        |> Enum.map(&(Enum.at(&1, 0)))
+        |> Enum.join()
+    end
+
+    defp collate(a_line, result) do
+        a_line
+            |> Stream.with_index(0)
+            |> Enum.reduce(result, &count_char_at/2)
+    end
+
+    defp count_char_at({character, index}, result) do
+      previous = Map.get(result, index, [])
+      new_count = previous ++ [character]
+      Map.put(result, index, new_count)
+    end
+
+    defp count(list) do
+      list
+      |> Enum.sort
+      |> Enum.chunk_by(&(&1))
+      |> Enum.sort(&(Enum.count(&1)<Enum.count(&2)))
+      |> Enum.map(&(Enum.at(&1, 0)))
+    end
+
+
+  def read_file(filename) do
+    case File.read("lib/day06/#{filename}") do
+      {:ok, content} -> content |> String.trim |> String.split("\n")
+      {:error, _} -> raise "Input file is missing"
+    end
+  end
+
+end
